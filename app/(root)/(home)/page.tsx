934d7bb349e0e1a0383701e01654ccb6b58c3973
Ashutosh Pawar <pawarash000@outlook.com>
2024-04-02 22:32:09 +0530

    md device fixed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='w-full h-[300px] rounded-[20px] bg-hero bg-cover'>
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 md:p-8 lg:p-11">
           <h2 className='max-w-[270px] bg-white bg-opacity-10 rounded py-2 text-center text-base font-normal'>Upcoming Meeting at: 12:09 PM</h2>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold lg:text-7xl'>{time}</h1>
